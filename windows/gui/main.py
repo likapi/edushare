ed7d9a4fef33e8329354ffa9b155da7d5ee54e0b
thisiskeanyvy <kinvestisseur@gmail.com>
2021-03-29 20:54:51 +0200

    ajout de l'installateur windows

--- windows/gui/main.py
+++ windows/gui/main.py
@@ -3,5 +3,6 @@ from tkinter import *
 window = Tk(screenName=None, baseName=None, className=' EduShare', useTk=1)
 window.geometry("500x400")
 window.configure(bg="#5b3c11")
+window.iconbitmap('assets/logo.ico')
 
 window.mainloop()
